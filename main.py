120ec56136f13594c730d48abdf6d18210b2f201
jackson-jackson <jackson-jackson@users.noreply.github.com>
2019-04-23 18:35:01 -0700

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import time
 polo = poloniex.Poloniex(private.API_KEY,private.API_SECRET)
 
 min_order = .15 #TODO make this based on min btc order size of .0001
-one_week = 604800 #TODO use a better variable name
+time_to_sell = 350 #TODO use a better variable name
 
 token = 'ATOM'
 currency_pair = 'BTC_ATOM'
@@ -22,10 +22,10 @@ def check_balance(token):
 # Determine number or orders based on the minimum order size
 def number_of_orders(token):
     token_balance = check_balance(token)    
-    if float(token_balance) > min_order:
-        number_of_orders = math.floor(token_balance) / min_order
+    if token_balance > min_order:
+        number_of_orders = math.floor(token_balance / min_order)
         return number_of_orders #TODO Optimize by returning token balance as well
-
+    return 0
 
 # Get average price of currency pair
 def get_average_price(currency_pair):
@@ -44,19 +44,17 @@ def get_average_price(currency_pair):
 def sell_token(token):
     token_balance = check_balance(token)
     print(f"Balance: {token_balance}")
-    orders = number_of_orders(token) + 1
+    orders = number_of_orders(token)
     print(f"Number of Orders: {orders}")
-    count = 1
+    count = 0
     # Determine the sell interval, in seconds, required to sell entire balance over one week
-    sell_interval = 10
-    # sell_interval = math.floor(one_week / number_of_orders(token))
-    print(f"Sell Interval in Seconds: {sell_interval}")
-    while count <= 2:
+    sell_interval = math.floor(time_to_sell / number_of_orders(token))
+    while count < 2:
         price = get_average_price(currency_pair)
         polo.sell(currency_pair, price, min_order) # DANGER DANGER DANGER
-        print(f"Order {count} of {orders}")
+        print(f"Order {count + 1} of {orders}")
         count += 1
-        print(f"Sleeping for {time.sleep(sell_interval)}")
+        print(f"Sleeping for {sell_interval}")
         time.sleep(sell_interval)
 
         #TODO add error handling
