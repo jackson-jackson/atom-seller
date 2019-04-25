d33ce2c6ee17e9916b081c6ed6d8bbbdcd42073f
jackson-jackson <jackson-jackson@users.noreply.github.com>
2019-04-25 16:03:24 -0700

    refactoring

--- main.py
+++ main.py
@@ -1,35 +1,23 @@
-import poloniex
-import private
+import settings
 from currencies import Bitcoin, Atom
 import math
 import time
 import threading
 from os import system
 
-
-polo = poloniex.Poloniex(private.API_KEY,private.API_SECRET)
-
-# Configuration
-target_volume_day = 10 # Target amount of atoms that the script should sell per day
-min_atom_price = 3 # Minimum atom price in USD that the script should sell at. Script will not sell below this
-
-min_order = .25 #TODO make this based on min btc order size of .0001
-
-currency_pair = 'BTC_ATOM'
-
 amount_sold = 0.0
 num_orders = 0
-
 
 # Work out rate for orders
 def order_velocity():
     day = 24 * 60 * 60
-    num_orders = target_volume_day / min_order
+    num_orders = settings.TARGET_VOLUME_DAY / settings.MIN_ORDER
     velocity = day / num_orders
 
     return velocity
 
 
+# Get updated balances
 def update_balances():
     Bitcoin.balance()
     Atom.balance()
@@ -37,7 +25,7 @@ def update_balances():
 
 # Get average price of currency pair
 def get_average_price(currency_pair):    
-    orderbook = polo.returnOrderBook(currency_pair, depth=5)
+    orderbook = settings.POLO.returnOrderBook(settings.CURRENCY_PAIR, depth=5)
     
     sum_price = 0
 
@@ -50,12 +38,12 @@ def get_average_price(currency_pair):
 def sell():
     global num_orders, amount_sold
 
-    price = get_average_price(currency_pair)
+    price = get_average_price(settings.CURRENCY_PAIR)
 
     if Atom.price() > price and Atom.balance() > 0.5: # Above min price
-        polo.sell(currency_pair, price, min_order)
+        settings.POLO.sell(settings.CURRENCY_PAIR, price, settings.MIN_ORDER)
         num_orders += 1
-        amount_sold += min_order
+        amount_sold += settings.MIN_ORDER
 
 
 def cli_update():
@@ -65,7 +53,7 @@ def cli_update():
     print(" ")
     print(f"Current prices: BTC ${round(Bitcoin.price(), 2)} USD; ATOM ${round(Atom.price() * Bitcoin.price(), 2)} USD")
     print(" ")
-    print(f"Target of {target_volume_day} atoms per day, with minimum price of ${min_atom_price} USD, and sell interval of {round(order_velocity())} seconds.")
+    print(f"Target of {settings.TARGET_VOLUME_DAY} atoms per day, with minimum price of ${settings.MIN_PRICE} USD, and sell interval of {round(order_velocity())} seconds.")
     print(" ")
     print(f"Current BTC balance is {round(Bitcoin.balance(), 2)} (${round(Bitcoin.balance() * Bitcoin.price(), 2)}), and ATOM balance is {round(Atom.balance(), 2)} (${round(Atom.balance() * Atom.price() * Bitcoin.price(), 2)})")
     print(" ")
---
+++ settings.py
@@ -0,0 +1,12 @@
+import poloniex
+import private
+
+POLO = poloniex.Poloniex(private.API_KEY,private.API_SECRET)
+
+# Configuration
+CURRENCY_PAIR = 'BTC_ATOM'
+TARGET_VOLUME_DAY = 10 # Target amount of atoms that the script should sell per day
+MIN_PRICE = 3 # Minimum atom price in USD that the script should sell at. Script will not sell below this
+MIN_ORDER = .25 #TODO make this based on min btc order size of .0001
+
+
