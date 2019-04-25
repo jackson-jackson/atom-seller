97e32ccb2c11fe6759d3d9d9f469f3969b9d088e
jackson-jackson <jackson-jackson@users.noreply.github.com>
2019-04-25 15:36:36 -0700

    add classes

---
+++ currencies.py
@@ -0,0 +1,44 @@
+import poloniex
+import private
+
+polo = poloniex.Poloniex(private.API_KEY,private.API_SECRET)
+
+def get_last_price(currency_pair):
+    tickers = polo.returnTicker()
+    last_price = float(tickers[currency_pair]['last'])
+    
+    return last_price
+
+
+class Bitcoin:
+    @staticmethod
+    def balance():
+        balance = polo.returnBalances()
+        balance = float(balance['BTC'])
+
+        return balance
+
+    @staticmethod
+    def price():
+        price = get_last_price('USDT_BTC')
+
+        return price
+        
+    prices = []
+
+
+class Atom:
+    @staticmethod
+    def balance():
+        balance = polo.returnBalances()
+        balance = float(balance['ATOM'])
+
+        return balance
+
+    @staticmethod
+    def price():
+        price = get_last_price('BTC_ATOM')
+
+        return price
+        
+    prices = []
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import poloniex
 import private
+from currencies import Bitcoin, Atom
 import math
 import time
 import threading
@@ -17,29 +18,12 @@ min_order = .25 #TODO make this based on min btc order size of .0001
 token = 'ATOM'
 currency_pair = 'BTC_ATOM'
 
-btc_price = 0.0 # btc price in USD
-atom_price = 0.0 # atom price in BTC
 amount_sold = 0.0
 num_orders = 0
 btc_prices = []
 atom_prices = []
 atom_balance = 0.0
 btc_balance = 0.0
-
-
-# Update BTC and ATOM prices
-def update_prices():
-    global btc_price, atom_price
-
-    btc_usd_book = polo.returnOrderBook('USDC_BTC', depth=3)
-    btc_price = float(btc_usd_book['bids'][0][0])
-
-    btc_prices.append(btc_price)
-
-    btc_atom_book = polo.returnOrderBook('BTC_ATOM', depth=3)
-    atom_price = float(btc_atom_book['bids'][0][0])
-
-    atom_prices.append(btc_price * atom_price)
 
 
 # Work out rate for orders
@@ -60,8 +44,7 @@ def update_balances():
 
 
 # Get average price of currency pair
-def get_average_price(currency_pair):
-    
+def get_average_price(currency_pair):    
     orderbook = polo.returnOrderBook(currency_pair, depth=5)
     
     sum_price = 0
@@ -77,30 +60,28 @@ def sell():
 
     price = get_average_price(currency_pair)
 
-    if atom_price > price and atom_balance > 0.5: # Above min price
+    if Atom.price() > price and atom_balance > 0.5: # Above min price
         polo.sell(currency_pair, price, min_order)
         num_orders += 1
         amount_sold += min_order
 
 
 def cli_update():
-
     system('cls') # clear for linux, cls for Windows
 
     print("Atom Seller")
     print(" ")
-    print(f"Current prices: BTC {round(btc_price, 2)}  ATOM {round(atom_price * btc_price, 2)}")
+    print(f"Current prices: BTC ${round(Bitcoin.price(), 2)} USD; ATOM ${round(Atom.price() * Bitcoin.price(), 2)} USD")
     print(" ")
     print(f"Target of {target_volume_day} atoms per day, with minimum price of ${min_atom_price} USD, and sell interval of {round(order_velocity())} seconds.")
     print(" ")
-    print(f"Current BTC balance is {round(btc_balance, 2)} (${round(btc_balance * btc_price, 2)}), and ATOM balance is {round(atom_balance, 2)} (${round(atom_balance * atom_price * btc_price, 2)})")
+    print(f"Current BTC balance is {round(btc_balance, 2)} (${round(btc_balance * Bitcoin.price(), 2)}), and ATOM balance is {round(atom_balance, 2)} (${round(atom_balance * Atom.price() * Bitcoin.price(), 2)})")
     print(" ")
     print(f"Number of orders executed: {num_orders}")
     print(f"Total atoms sold: {amount_sold}")
     
 
 def run_updates():
-    update_prices()
     update_balances()
     cli_update()
 
