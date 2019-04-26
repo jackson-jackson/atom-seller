29bb68b03cc36821b08bc04e6080b3c63ef6597e
jackson-jackson <jackson-jackson@users.noreply.github.com>
2019-04-25 18:53:30 -0700

    Update settings.py

--- settings.py
+++ settings.py
@@ -8,4 +8,3 @@ CURRENCY_PAIR = 'BTC_ATOM'
 TARGET_VOLUME_DAY = 10 # Target amount of atoms that the script should sell per day
 MIN_PRICE = 3 # Minimum atom price in USD that the script should sell at. Script will not sell below this
 MIN_ORDER = .00015 #TODO make this based on min btc order size of .0001
-OS = 'WINDOWS' # WINDOWS OR LINUX
