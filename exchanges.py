084369ffdbe1fa1557de70b2fef2938c380d0586
jackson-jackson <jackson-jackson@users.noreply.github.com>
2019-04-28 17:20:45 -0700

    added exchange classes

---
+++ exchanges.py
@@ -0,0 +1,129 @@
+import poloniex
+import krakenex
+import settings
+import private
+
+selected_exchange = settings.EXCHANGE
+
+
+class Exchange:
+    pass
+
+
+class Poloniex(Exchange):
+    def __init__(self, key, secret, currency_pair, ticker):
+        if not currency_pair:
+            raise TypeError("currency_pair is required")
+
+        self.polo = poloniex.Poloniex(key, secret)
+        self.currency_pair = currency_pair
+        self.ticker = ticker
+    
+
+    # Get lowest ask price on orderbook
+    def get_lowest_ask(self):
+        tickers = self.polo.returnTicker()
+        lowest_ask = tickers[self.currency_pair]['lowestAsk']
+
+        return float(lowest_ask)
+
+    
+    # Get highest bid price on orderbook
+    def get_highest_bid(self):
+        tickers = self.polo.returnTicker()
+        highest_bid = tickers[self.currency_pair]['highestBid']
+
+        return float(highest_bid)
+
+
+    # Get total number of currency in open orders
+    def get_open_orders(self):
+        orders = self.polo.returnOpenOrders(self.currency_pair)
+        
+        open_orders = []
+        
+        for order in orders:
+            open_orders.append(float(order['total']))
+
+        return sum(open_orders)
+
+
+    # Get sum total of all open orders for a given currency
+    def get_open_orders_total(self, currency_pair):
+        orders = self.polo.returnOpenOrders(currency_pair)
+        open_orders = []
+
+        for order in orders:
+            open_orders.append(float(order['total']))
+        return round(sum(open_orders), 8)
+
+
+    # Get currency_pair's last price
+    def get_last_price(self, currency_pair):
+        tickers = self.polo.returnTicker()
+        last_price = tickers[self.currency_pair]['last']
+    
+        return float(last_price)
+
+
+    # Return ticker balance
+    def get_balance(self, ticker):
+        balance = self.polo.returnBalances()
+        balance = balance[self.ticker]
+
+        return float(balance)
+
+
+    # Return currency_pair price in USD
+    def get_usd_price(self):
+        tickers = self.polo.returnTicker()
+        last_price = tickers['USDT_BTC']['last']
+    
+        return float(last_price)
+
+
+    # Sell 
+    def sell(self, currency_pair, price, min_order):
+        polo.sell(currency_pair, price, min_order)
+
+
+polo = Poloniex(private.API_KEY[selected_exchange], private.API_SECRET[selected_exchange], settings.CURRENCY_PAIR, settings.TICKER)
+
+
+
+class Kraken(Exchange):
+    def __init__(self, key, secret, currency_pair, ticker):
+        if not currency_pair:
+            raise TypeError("currency_pair is required")
+
+        self.kraken = krakenex.API(key, secret)
+        self.currency_pair = currency_pair
+        self.ticker = ticker
+    
+
+    # Get lowest ask price on orderbook
+    def get_lowest_ask(self):
+        ticker = self.kraken.query_public('Ticker', {'pair': self.currency_pair})
+        lowest_ask = ticker['result']['ATOMXBT']['a'][0]
+
+        return float(lowest_ask)
+
+
+    # Get highest ask price on orderbook
+    def get_highest_bid(self):
+        ticker = self.kraken.query_public('Ticker', {'pair': self.currency_pair})
+        highest_bid = ticker['result']['ATOMXBT']['b'][0]
+
+        return float(highest_bid)
+
+    
+    # Get total number of currency in open orders #TODO Test this with orders on Kraken
+    def get_open_orders(self):
+        orders = self.kraken.query_private('OpenOrders', {'trades': 'status'})
+    
+        # open_orders = []
+        
+        # for order in orders:
+        #     open_orders.append(float(order['total']))
+    
+        return orders
--- main.py
+++ main.py
@@ -1,22 +1,46 @@
 import settings
+import poloniex
 import private
-import functions
-from currencies import Bitcoin, Atom
+from exchanges import Poloniex, Kraken
 import math
 import time
 import threading
 from os import system
 
+
 amount_sold = 0.0
 num_orders = 0
 open_orders_total = 0.0
+exch = None
+selected_exchange = settings.EXCHANGE
+
+
+# Check which exchange to use
+if selected_exchange == 'POLO':
+    exch = Poloniex(private.API_KEY[selected_exchange], private.API_SECRET[selected_exchange], settings.CURRENCY_PAIR, settings.TICKER)
+elif selected_exchange == 'KRAKEN':
+    exch = Kraken(private.API_KEY[selected_exchange], private.API_SECRET[selected_exchange], settings.CURRENCY_PAIR, settings.TICKER)
+
+    
+# Get price inside spread
+def price_inside_spread():
+    highest_bid = exch.get_highest_bid()
+    lowest_ask = exch.get_lowest_ask()
+
+    spread = lowest_ask - highest_bid
+
+    order_price = highest_bid + (spread / 2)
+
+    return order_price
+
+print(f"price inside spread: {exch.get_open_orders()}")
 
 
 # Work out rate for orders
 def order_velocity():
     day = 24 * 60 * 60
 
-    price = price_inside_spread(settings.CURRENCY_PAIR)
+    price = price_inside_spread()
 
     # Determine min order
     min_order = settings.MIN_ORDER / price
@@ -27,52 +51,30 @@ def order_velocity():
     return velocity
 
 
-# Get price inside spread
-def price_inside_spread(currency_pair):
-        tickers = settings.POLO.returnTicker()
-        highest_bid = float(tickers[settings.CURRENCY_PAIR]['highestBid'])
-        lowest_ask = float(tickers[settings.CURRENCY_PAIR]['lowestAsk'])
-        
-        spread = lowest_ask - highest_bid
-
-        order_price = highest_bid + (spread / 2)
-
-        return round(order_price, 8)
-
-
-# Get total number of ATOM in open orders
-def get_open_orders_total():
-    orders = settings.POLO.returnOpenOrders('BTC_ATOM')
-    open_orders = []
-
-    for order in orders:
-        open_orders.append(float(order['total']))
-    return round(sum(open_orders), 8)
-
-
 # Place limit order
 def place_order():
     global num_orders, amount_sold
 
-    price = price_inside_spread(settings.CURRENCY_PAIR)
+    price = price_inside_spread()
 
     # Determine min order
     min_order = settings.MIN_ORDER / price
 
-    if Atom.price() > price and Atom.balance() > 0.5:
-        settings.POLO.sell(settings.CURRENCY_PAIR, price, min_order)
+    if float(exch.get_last_price(settings.CURRENCY_PAIR)) > price and float(exch.get_balance(settings.SELLING)) > 0.5:
+        exch.sell(settings.CURRENCY_PAIR, price, min_order)
         num_orders += 1
         amount_sold += min_order
 
 
+# Live CLI updates
 def cli_update():
-    btc_price = Bitcoin.price()
-    btc_balance = Bitcoin.balance()
-    atom_balance = Atom.balance()
-    atom_price = Atom.price()
+    btc_price = float(exch.get_usd_price()) #TODO make variable name currency agnostic
+    btc_balance = exch.get_balance(settings.TICKER) #TODO make variable name currency agnostic
+    atom_balance = float(exch.get_balance(settings.SELLING)) #TODO make variable name currency agnostic
+    atom_price = float(exch.get_last_price(settings.CURRENCY_PAIR)) #TODO make variable name currency agnostic
 
-    open_orders = functions.open_orders()
-    open_orders_total = get_open_orders_total()
+    open_orders = exch.get_open_orders()
+    open_orders_total = exch.get_open_orders_total(settings.CURRENCY_PAIR)
     order_vel = order_velocity()
 
     if private.OS == 'LINUX':
@@ -90,7 +92,7 @@ def cli_update():
     print(" ")
     print(f"Number of orders executed: {num_orders}")
     print(f"Total ATOM sold: {round(amount_sold, 8)}")
-    print(f"Number of open orders: {len(open_orders)}")
+    print(f"Number of open orders: {open_orders}")
     print(f"Total ATOM in open orders: {open_orders_total}")
     
 
